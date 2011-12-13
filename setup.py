7450d54c41090fdec0777da072c4803e2caa396c
Jim Fulton <jim@zope.com>
2011-12-13 20:12:41 +0000

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-name, version = 'zc.zkzeo', '0'
+name, version = 'zc.zkzeo', '0.2.0'
 
 install_requires = [
     'setuptools', 'zc.zk [static]', 'ZODB3', 'zc.thread']
